ddc9157d062cdea51f13d0ba8911c973702e905a
Jon Duenas <51519363+jonduenas@users.noreply.github.com>
2023-09-15 15:33:14 -0400

    Initial Commit

---
+++ DependenciesTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DependenciesTest
+//
+//  Created by Jon Duenas on 9/15/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DependenciesTest/DependenciesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependenciesTestApp.swift
+//  DependenciesTest
+//
+//  Created by Jon Duenas on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct DependenciesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
